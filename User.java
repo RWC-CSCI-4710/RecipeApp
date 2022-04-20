b59fad1cbeb6fc3e22fc4a94bc1d10c091e2dc66
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-04-20 10:59:57 -0400

    Clean up User.java
    Renamed parameters to be the same as the User variables

--- User.java
+++ User.java
@@ -10,25 +10,25 @@ public class User {
     String userID;
     String password;
     
-    public User(String inUserName, String inUserEmail, String inFullName, String inUserID, String inPassword){
-        userName = inUserName;
-        userEmail = inUserEmail;
-        fullName = inFullName;
-        userID = inUserID;
-        password = inPassword;
+    public User(String userName, String userEmail, String fullName, String userID, String password){
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.fullName = fullName;
+        this.userID = userID;
+        this.password = password;
     }
 
-    public String getUserName(){
+    public String getUserName() {
         return userName;
     }
-    public String getUserEmail(){
+    public String getUserEmail() {
         return userEmail;
     }
-    public String getFullName(){
+    public String getFullName() {
 
         return fullName;
     }
-    public String getUserID(){
+    public String getUserID() {
         return userID;
     }
 }
