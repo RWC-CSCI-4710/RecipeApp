5825c05b94c07e69825913e6d685b6ef2943e1cc
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-04-21 12:57:19 -0400

    Rename ManagerRecipe class to RecipeManager
    Add empty addRecipe and removeRecipe functions

---
+++ RecipeApp/src/main/java/rwc/csci4710/recipeapp/RecipeManager.java
@@ -0,0 +1,15 @@
+package rwc.csci4710.recipeapp;
+
+/**
+ *
+ * @author liberto_vincent
+ */
+public class RecipeManager {
+    public void addRecipe(User user, Recipe recipe) {
+        // Add recipe to database...
+    }
+    
+    public void removeRecipe(User user, Recipe recipe) {
+        // Remove recipe from database...
+    }
+}
