27b67d3e852b8a6ba368a620287620fb68eb1450
Gibbons <chuck@10.85.5.208>
2022-04-21 16:55:22 -0400

    Updated Firebase Initialize

--- RecipeApp/src/main/java/rwc/csci4710/recipeapp/FirebaseInit.java
+++ RecipeApp/src/main/java/rwc/csci4710/recipeapp/FirebaseInit.java
@@ -5,6 +5,11 @@ import com.google.firebase.FirebaseApp;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 
 /**
@@ -12,6 +17,8 @@ import java.io.IOException;
  * @author Jared Gibbons
  */
 public class FirebaseInit {
+    
+    DatabaseReference refR;
     
     public void initialize() throws FileNotFoundException, IOException{
         
@@ -24,5 +31,20 @@ public class FirebaseInit {
             .build();
 
         FirebaseApp.initializeApp(options);
+        
+        // As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+            .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+          @Override
+          public void onDataChange(DataSnapshot dataSnapshot) {
+            Object document = dataSnapshot.getValue();
+            System.out.println(document);
+          }
+
+          @Override
+          public void onCancelled(DatabaseError error) {
+          }
+        });
     }
 }
