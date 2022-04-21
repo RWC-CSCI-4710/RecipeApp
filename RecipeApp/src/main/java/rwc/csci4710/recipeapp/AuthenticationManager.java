728edc127b44080327d2e8efd0e5dee305b0815f
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-04-21 13:11:58 -0400

    Add AuthenticationManager class
    Add empty registerUser function

---
+++ RecipeApp/src/main/java/rwc/csci4710/recipeapp/AuthenticationManager.java
@@ -0,0 +1,11 @@
+package rwc.csci4710.recipeapp;
+
+/**
+ *
+ * @author mattx
+ */
+public class AuthenticationManager {
+    public void registerUser() {
+        // Register a new User...
+    }
+}
