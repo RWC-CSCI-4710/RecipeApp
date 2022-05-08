e2929db32edd076428220f69fe45bf5056637aa8
liberto_vincent <liberto_vincent@LAB-SH-358-14.roberts.edu>
2022-05-08 18:57:43 -0400

    Register Button Added

--- RecipeApp/src/main/java/rwc/csci4710/recipeapp/LoginFrame.java
+++ RecipeApp/src/main/java/rwc/csci4710/recipeapp/LoginFrame.java
@@ -186,7 +186,7 @@ public class LoginFrame extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         new RegisterFrame().setVisible(true);
-        this.dispose();
+        this.dispose(); 
     }//GEN-LAST:event_jButton1ActionPerformed
     
     public boolean verify(String inUsername, String inPassword) throws FileNotFoundException{
